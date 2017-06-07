52351e7f4d207dd303e5f0ac1f73e0929b8b1618
Armstrong Subero <armstrongsubero@gmail.com>
2017-06-07 14:22:06 -0800

    Add files via upload

---
+++ 09_Low_Power.X/mcc_generated_files/uart1.c
@@ -0,0 +1,131 @@
+/**
+  UART1 Generated Driver File
+
+  @Company
+    Microchip Technology Inc.
+
+  @File Name
+    uart1.c
+
+  @Summary
+    This is the generated driver implementation file for the UART1 driver using MPLAB(c) Code Configurator
+
+  @Description
+    This header file provides implementations for driver APIs for UART1.
+    Generation Information :
+        Product Revision  :  MPLAB(c) Code Configurator - pic24-dspic-pic32mm : v1.25
+        Device            :  PIC24FJ128GB204
+        Driver Version    :  2.00
+    The generated drivers are tested against the following:
+        Compiler          :  XC16 1.26
+        MPLAB             :  MPLAB X 3.45
+*/
+
+/*
+    (c) 2016 Microchip Technology Inc. and its subsidiaries. You may use this
+    software and any derivatives exclusively with Microchip products.
+
+    THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+    EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
+    WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
+    PARTICULAR PURPOSE, OR ITS INTERACTION WITH MICROCHIP PRODUCTS, COMBINATION
+    WITH ANY OTHER PRODUCTS, OR USE IN ANY APPLICATION.
+
+    IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+    INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+    WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
+    BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
+    FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
+    ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+    THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+
+    MICROCHIP PROVIDES THIS SOFTWARE CONDITIONALLY UPON YOUR ACCEPTANCE OF THESE
+    TERMS.
+*/
+
+/**
+  Section: Included Files
+*/
+#include "uart1.h"
+
+/**
+  Section: UART1 APIs
+*/
+
+void UART1_Initialize(void)
+{
+/**    
+     Set the UART1 module to the options selected in the user interface.
+     Make sure to set LAT bit corresponding to TxPin as high before UART initialization
+*/
+    // STSEL 1; IREN disabled; PDSEL 8N; UARTEN enabled; RTSMD disabled; USIDL disabled; WAKE disabled; ABAUD disabled; LPBACK disabled; BRGH enabled; URXINV disabled; UEN TX_RX; 
+    U1MODE = (0x8008 & ~(1<<15));  // disabling UARTEN bit   
+    // UTXISEL0 TX_ONE_CHAR; UTXINV disabled; OERR NO_ERROR_cleared; URXISEL RX_ONE_CHAR; UTXBRK COMPLETED; UTXEN disabled; ADDEN disabled; 
+    U1STA = 0x0000;
+    // BaudRate = 9600; Frequency = 16000000 Hz; U1BRG 416; 
+    U1BRG = 0x01A0;
+    // ADMADDR 0; ADMMASK 0; 
+    U1ADMD = 0x0000;
+    // T0PD 1 ETU; PTRCL T0; TXRPT Retransmits the error byte once; CONV Direct; SCEN disabled; 
+    U1SCCON = 0x0000;
+    // TXRPTIF disabled; TXRPTIE disabled; WTCIF disabled; WTCIE disabled; PARIE disabled; GTCIF disabled; GTCIE disabled; RXRPTIE disabled; RXRPTIF disabled; 
+    U1SCINT = 0x0000;
+    // GTC 0; 
+    U1GTC = 0x0000;
+    // WTCL 0; 
+    U1WTCL = 0x0000;
+    // WTCH 0; 
+    U1WTCH = 0x0000;
+    
+    U1MODEbits.UARTEN = 1;  // enabling UARTEN bit
+    U1STAbits.UTXEN = 1; 
+   
+}
+
+
+uint8_t UART1_Read(void)
+{
+    while(!(U1STAbits.URXDA == 1))
+    {
+        
+    }
+
+    if ((U1STAbits.OERR == 1))
+    {
+        U1STAbits.OERR = 0;
+    }
+
+    
+
+    return U1RXREG;
+}
+
+void UART1_Write(uint8_t txData)
+{
+    while(U1STAbits.UTXBF == 1)
+    {
+        
+    }
+
+    U1TXREG = txData;    // Write the data byte to the USART.
+}
+
+UART1_STATUS UART1_StatusGet (void)
+{
+    return U1STA;
+}
+
+int __attribute__((__section__(".libc.write"))) write(int handle, void *buffer, unsigned int len) {
+    int i;
+    while(U1STAbits.TRMT == 0);  
+    for (i = len; i; --i)
+    {
+        while(U1STAbits.TRMT == 0);
+        U1TXREG = *(char*)buffer++;        
+    }
+    return(len);
+}
+
+/**
+  End of File
+*/
