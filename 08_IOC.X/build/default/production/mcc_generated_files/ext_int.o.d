18841767f781d90d6bd574168adedcfffee535d1
Armstrong Subero <armstrongsubero@gmail.com>
2017-06-07 14:20:38 -0800

    Add files via upload

---
+++ 08_IOC.X/build/default/production/mcc_generated_files/ext_int.o.d
@@ -0,0 +1,3 @@
+build/default/production/mcc_generated_files/ext_int.o:  \
+ mcc_generated_files/ext_int.c  \
+mcc_generated_files/ext_int.h 
---
+++ 08_IOC.X/mcc_generated_files/ext_int.c
@@ -0,0 +1,87 @@
+/**
+  EXT_INT Generated Driver File 
+
+  @Company:
+    Microchip Technology Inc.
+
+  @File Name:
+    ext_int.c
+
+  @Summary
+    This is the generated driver implementation file for the EXT_INT 
+    driver using MPLAB(c) Code Configurator
+
+  @Description:
+    This source file provides implementations for driver APIs for EXT_INT. 
+    Generation Information : 
+        Product Revision  :  MPLAB(c) Code Configurator - pic24-dspic-pic32mm : v1.25
+        Device            :  PIC24FJ128GB204
+        Driver Version    :  1.0
+    The generated drivers are tested against the following:
+        Compiler          :  XC16 1.26
+        MPLAB             :  MPLAB X 3.45
+*/
+
+/**
+   Section: Includes
+ */
+#include <xc.h>
+#include "ext_int.h"
+//***User Area Begin->code: Add External Interrupt handler specific headers 
+
+//***User Area End->code: Add External Interrupt handler specific headers
+
+/**
+   Section: External Interrupt Handlers
+ */
+/**
+  Interrupt Handler for EX_INT1 - INT1
+*/
+void __attribute__ ( ( interrupt, no_auto_psv ) ) _INT1Interrupt(void)
+{
+    //***User Area Begin->code: INT1 - External Interrupt 1***
+
+    //***User Area End->code: INT1 - External Interrupt 1***
+    EX_INT1_InterruptFlagClear();
+}
+/**
+  Interrupt Handler for EX_INT0 - INT0
+*/
+void __attribute__ ( ( interrupt, no_auto_psv ) ) _INT0Interrupt(void)
+{
+    //***User Area Begin->code: INT0 - External Interrupt 0***
+   
+    //***User Area End->code: INT0 - External Interrupt 0***
+    EX_INT0_InterruptFlagClear();
+}
+/**
+    Section: External Interrupt Initializers
+ */
+/**
+    void EXT_INT_Initialize(void)
+
+    Initializer for the following external interrupts
+    INT1
+    INT0
+*/
+void EXT_INT_Initialize(void)
+{
+    /*******
+     * INT1
+     * Clear the interrupt flag
+     * Set the external interrupt edge detect
+     * Enable the interrupt, if enabled in the UI. 
+     ********/
+    EX_INT1_InterruptFlagClear();   
+    EX_INT1_PositiveEdgeSet();
+    EX_INT1_InterruptEnable();
+    /*******
+     * INT0
+     * Clear the interrupt flag
+     * Set the external interrupt edge detect
+     * Enable the interrupt, if enabled in the UI. 
+     ********/
+    EX_INT0_InterruptFlagClear();   
+    EX_INT0_PositiveEdgeSet();
+    EX_INT0_InterruptEnable();
+}
